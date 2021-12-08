74e36e17740aedd0de69307a0b80334e256173dc
Naoya <86583481+naonaonao708@users.noreply.github.com>
2021-12-08 19:12:39 +0900

    12/8

--- chapter5.R
+++ chapter5.R
@@ -10,3 +10,19 @@ df[,'psych_test']
 z_denominator <- mean(df[,'psych_test'])-12 # z値の分子を計算
 z_numerator <- sqrt(10/length(df[,'psych_test'])) #z値の分母を計算
 z_value <- z_denominator/z_numerator
+
+# qnorm(p)：第一引数に確率を受けて、確率密度を返す
+qnorm(0.025) # 下側確率0.025となるz値を求める→両側検定・有意水準５％
+qnorm(0.975) # 下側確率0.975となるz値
+
+qnorm(0.025, lower.tail = FALSE) #上側確率0.025のz値
+
+# 今回の棄却域であるZ＜pnorm(0.025),Z＞pnorm(0.975)にZ値が入るため、帰無仮説は棄却される
+
+# pnorm(q)：第一引数に確率密度を受けて、確率を返す
+pnorm(-2.828427) # 下側確率Prob(z=<-2.828427)
+pnorm(2.828427,lower.tail = FALSE) # 上側確率Prob(z>2.828427)
+
+2*pnorm(2.82847,lower.tail = FALSE) # 両側検定なので＊２
+# 値が0.004677108と有意水準である0.05よりも小さいので帰無仮説は棄却される
+
